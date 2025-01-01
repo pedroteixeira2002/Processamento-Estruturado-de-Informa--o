db3d5d95e867f31e4e2087fd76977d3f3abfdb55
pedroteixeira2002 <8200489@estg.ipp.pt>
2025-01-01 18:49:05 +0000

    update

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-require('dotenv').config();
 
 const clinicalRoutes = require('./routes/ClinicalRecordRoute');
 const transferRoutes = require('./routes/TransferRoute');
@@ -10,14 +11,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    res.send('Servidor na porta 3000!');
+});
+
 app.use('/api/clinical-reports', clinicalRoutes);
 app.use('/api/transfer-reports', transferRoutes);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Mongo conectado'))
     .catch((err) => console.error('Erro ao conectar ao Mongo: ', err));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor na porta ${PORT}`);
-});
+
+module.exports = app;
--- server/routes/ClinicalRecordRoute.js
+++ server/routes/ClinicalRecordRoute.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { getClinicalReportByMonth } = require('../controllers/clinicalRecordController');
+const  ClinicalReport = require('../controllers/clinicalRecordController.js');
 
 const router = express.Router();
 
-router.get('/:year/:month', getClinicalReportByMonth);
+router.get('/:year/:month', ClinicalReport.getClinicalReportByMonth);
 
 module.exports = router;
